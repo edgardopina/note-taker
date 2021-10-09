a6ba200a80a4f48c521be62ee579bc15694c6351
edgardopina <edgardopina@yahoo.com>
2021-10-08 22:36:18 -0600

    Added heroku process.env.PORT var and deployed to heroku

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
+
+// use heroku environment variable (port 80/443) if it is set OR use 3001
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { notes } = require('./Develop/db/db.json');
+
+function filterByQuery(query, notesArray) {
+   let filteredResults = notesArray;
+   if (query.title) {
+      filteredResults = filteredResults.filter((note) => note.title === query.title);
+   }
+   return filteredResults;
+}
 
 app.get('/api/notes', (req, res) => {
    let results = notes;
    console.log(req.query);
-   res.json(results); Cajamagiuca232021
-   
+   if (req.query) {
+      results = filterByQuery(req.query, results);
+      // results = results;
+   }
+   res.json(results);
 });
 
-
-app.listen(3001, () => {
-   console.log(`API server now on port 3001`);
+app.listen(PORT, () => {
+   console.log(`API server now on port ${PORT}`);
 });
